0e3c37edb30f1fa864b70df2b6f8f503e539d584
Natang Atul Salgia <natang.salgia@gmail.com>
2013-11-26 11:07:43 -0800

    FIXED BACKWARD CHAINING

--- src/BackwardChaining.java
+++ src/BackwardChaining.java
@@ -1,99 +1,68 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class BackwardChaining extends Inference {
 
-	public BackwardChaining(File kb, File q, File oe, File ol) {
-		super(kb, q, oe, ol);
-	}
+    public BackwardChaining(File kb, File q, File oe, File ol) {
+        super(kb, q, oe, ol);
+    }
 
-	private String getAND(ArrayList<String> ip) {
-		StringBuilder sb = new StringBuilder();
-		for (String string : ip) {
-			sb.append((string + "&"));
-		}
-		String string = sb.toString();
-		return (string.replaceAll("&$", ""));
-	}
+    @Override
+    protected boolean impl(String q) {
+        List<String> facts = Helper.cloneList(this.facts);
+        List<String> entailed = Helper.cloneList(this.entailed);
+        List<Rule> rules = Helper.cloneList(this.rules);
+        for (int i = 0; i < rules.size(); i++) {
+            Rule rule = rules.get(i);
+            rules.set(i, new Rule(rule.getLhs(), rule.getRhs(), rule.getCount()));
+        }
+        List<String> tasks = new ArrayList<String>();
+        tasks.add(q);
+        LinkedHashMap<String, LinkedList<Rule>> branchMap = new LinkedHashMap<String, LinkedList<Rule>>();
 
-	@Override
-	protected boolean impl(String q) {
-		Map<String, Boolean> entailValue = new HashMap<String, Boolean>();
-		String clauseStr = "";
+        while (!tasks.isEmpty()) {
+            String task = tasks.remove(0);
+            entailed.add(task);
+            if (!facts.contains(task)) {
+                LinkedList<Rule> ruleList = new LinkedList<Rule>();
+                Rule selectedRule = null;
+                for (Rule rule : rules) {
+                    if (rule.getRhs().equalsIgnoreCase(task)) {
+                        ruleList.add(rule);
+                    }
+                }
+                branchMap.put(task, ruleList);
+                int index = entailed.size() - 1;
+                task = entailed.get(index);
+                while (index >= 0 && (task = entailed.get(index)) != null
+                        && ((ruleList = branchMap.get(task)) == null || ruleList.isEmpty())) {
+                    index--;
+                }
 
-		List<String> facts = Helper.cloneList(this.facts);
-		List<String> entailed = Helper.cloneList(this.entailed);
-		List<Rule> rules = Helper.cloneList(this.rules);
-		for (int i = 0; i < rules.size(); i++) {
-			Rule rule = rules.get(i);
-			rules.set(i,
-					new Rule(rule.getLhs(), rule.getRhs(), rule.getCount()));
-		}
-		List<String> tasks = new ArrayList<String>();
-		tasks.add(q);
-		while (!tasks.isEmpty()) {
-			String task = tasks.remove(tasks.size() - 1);
-			entailed.add(task);
-			if (!facts.contains(task)) {
-				boolean ruleFound = false;
-				for (Rule rule : rules) {
-					if (rule.getRhs().equalsIgnoreCase(task)) {
-						ruleFound = true;
-						ArrayList<String> lhs = rule.getLhs();
-						String andLHS = getAND(lhs);
-						clauseStr += (andLHS + "/");
-						for (String string : lhs) {
-							System.out.println("adding "+string);
-							if (!entailed.contains(string)) {
-								tasks.add(string);
-							}
-						}
-					}
-				}
-				if (tasks.isEmpty() && !ruleFound) {
-					return false;
-				}
-				entailValue.put(task, ruleFound);
-				clauseStr = clauseStr.replaceAll("/$", "&");
-			} else {
-				entailValue.put(task, true);
-			}
-		}
-		clauseStr = clauseStr.replaceAll("&$", "");
-		System.out.println(clauseStr);
-		int length = clauseStr.length();
-		boolean op = entailValue.get(clauseStr.charAt(0) + "");
-		for (int i = 0; i < length - 2; i++) {
-			char c1 = clauseStr.charAt(i);
-			if (c1 == '&' || c1 == '/') {
-				continue;
-			}
-			char c2 = clauseStr.charAt(i + 2);
-			boolean op1 = entailValue.get(c1 + "");
-			boolean op2 = entailValue.get(c2 + "");
-			boolean temp = getBooleanValue(op1, op2, clauseStr, i + 1);
-			op = getBooleanValue(op, temp, clauseStr, i - 1);
-		}
-		return op;
-	}
+                if (ruleList == null || ruleList.isEmpty()) {
+                    return false;
+                }
 
-	private boolean getBooleanValue(boolean op1, boolean op2, String clauseStr,
-			int i) {
-		if (i < 0) {
-			return op2;
-		}
-		if (clauseStr.charAt(i) == '&') {
-			return op1 && op2;
-		}
-		return op1 || op2;
-	}
+                selectedRule = ruleList.removeFirst();
+                ArrayList<String> lhs = selectedRule.getLhs();
+                for (String string : lhs) {
+                    if (!entailed.contains(string)) {
+                        tasks.add(string);
+                    }
+                }
+            } else {
+                // print log for fact
+            }
+        }
+        return true;
+    }
 
-	@Override
-	protected String createLog(List<String> facts, Rule r) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected String createLog(List<String> facts, Rule r) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
